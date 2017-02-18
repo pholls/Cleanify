9ffc1f8aa528eb2aa4f6cbbb1875f637656c4247
Jacob M. Kelly <jakemkelly84@gmail.com>
2017-02-18 13:18:27 -0800

    Correct migration for badges_users

--- db/migrate/20170218211738_create_badges_users.rb
+++ db/migrate/20170218211738_create_badges_users.rb
@@ -1,8 +1,8 @@
 class CreateBadgesUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :badges_users do |t|
-      t.badge_id :integer
-      t.user_id :integer
+      t.integer :badge_id
+      t.integer :user_id
 
       t.timestamps
     end
