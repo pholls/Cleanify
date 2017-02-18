6d555f5ebd2d8e97d52e35d078bb1b48d3046abe
Jacob M. Kelly <jakemkelly84@gmail.com>
2017-02-18 12:40:32 -0800

    Update badges and badges_user models with validations, associations

--- db/migrate/20170218203107_create_badges.rb
+++ db/migrate/20170218203107_create_badges.rb
@@ -1,9 +1,9 @@
 class CreateBadges < ActiveRecord::Migration[5.0]
   def change
     create_table :badges do |t|
-      t.string :name
-      t.string :description
-      t.string :image
+      t.string :name, null: false
+      t.string :description, null: false
+      t.integer :image, null: false
 
       t.timestamps
     end
